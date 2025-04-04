55f5b8dce0abe768fb369fb76e95649917417dff
Romeu <pessoal@romeu.dev.br>
2025-04-04 17:33:25 -0300

    ajustes em despachos

--- app/controllers/despachos_controller.rb
+++ app/controllers/despachos_controller.rb
@@ -1,7 +1,7 @@
 class DespachosController < ApplicationController
   before_action :authenticate_user!
   before_action :set_despacho, only: %i[ show edit update destroy ]
-  before_action :set_armeiros_guardas_armas, only: %i[new edit]
+  before_action :set_armeiros_guardas_armas, only: %i[new edit create update]
 
   # GET /despachos or /despachos.json
   def index
@@ -15,6 +15,24 @@ class DespachosController < ApplicationController
   # GET /despachos/new
   def new
     @despacho = Despacho.new
+    @despacho.tipo = params[:tipo] if params[:tipo].present?
+    
+    # Pre-fill form for returns if an arma_id is provided
+    if params[:arma_id].present? && @despacho.tipo == "Devolução"
+      arma = Arma.find_by(id: params[:arma_id])
+      if arma && arma.emprestada?
+        # Get the last loan for this weapon
+        ultimo_emprestimo = Despacho.where(arma_id: arma.id, tipo: "Empréstimo").order(created_at: :desc).first
+        
+        if ultimo_emprestimo.present?
+          @despacho.arma_id = arma.id
+          @despacho.guarda_id = arma.guarda_atual_id
+          @despacho.quantidade_balas = ultimo_emprestimo.quantidade_balas
+          @despacho.quantidade_carregadores = ultimo_emprestimo.quantidade_carregadores
+          @despacho.calibre = ultimo_emprestimo.calibre
+        end
+      end
+    end
   end
 
   # GET /despachos/1/edit
@@ -24,9 +42,22 @@ class DespachosController < ApplicationController
   # POST /despachos or /despachos.json
   def create
     @despacho = Despacho.new(despacho_params)
+    
+    # Set matricula and porte values from associated records if not set in the form
+    if @despacho.armeiro_id.present? && @despacho.matricula_armeiro.blank?
+      armeiro = Guarda.find(@despacho.armeiro_id)
+      @despacho.matricula_armeiro = armeiro.matricula
+    end
+    
+    if @despacho.guarda_id.present?
+      guarda = Guarda.find(@despacho.guarda_id)
+      @despacho.matricula_guarda = guarda.matricula if @despacho.matricula_guarda.blank?
+      @despacho.porte_guarda = guarda.porte_arma if @despacho.porte_guarda.blank?
+    end
 
     respond_to do |format|
-      if @despacho.save
+      # Use save_with_arma_update instead of save
+      if @despacho.save_with_arma_update
         format.html { redirect_to @despacho, notice: "Despacho criado." }
         format.json { render :show, status: :created, location: @despacho }
       else
@@ -38,8 +69,24 @@ class DespachosController < ApplicationController
 
   # PATCH/PUT /despachos/1 or /despachos/1.json
   def update
+    # Similar logic for update
+    if despacho_params[:armeiro_id].present? && (despacho_params[:matricula_armeiro].blank? || @despacho.matricula_armeiro.blank?)
+      armeiro = Guarda.find(despacho_params[:armeiro_id])
+      params[:despacho][:matricula_armeiro] = armeiro.matricula
+    end
+    
+    if despacho_params[:guarda_id].present?
+      guarda = Guarda.find(despacho_params[:guarda_id])
+      params[:despacho][:matricula_guarda] = guarda.matricula if despacho_params[:matricula_guarda].blank?
+      params[:despacho][:porte_guarda] = guarda.porte_arma if despacho_params[:porte_guarda].blank?
+    end
+    
+    # Apply attributes but don't save yet
+    @despacho.assign_attributes(despacho_params)
+    
     respond_to do |format|
-      if @despacho.update(despacho_params)
+      # Use save_with_arma_update instead of update
+      if @despacho.save_with_arma_update
         format.html { redirect_to @despacho, notice: "Despacho atualizado." }
         format.json { render :show, status: :ok, location: @despacho }
       else
@@ -68,7 +115,13 @@ class DespachosController < ApplicationController
     def set_armeiros_guardas_armas
       @armeiros = Guarda.where(armeiro: true) 
       @guardas = Guarda.all 
-      @armas = Arma.all 
+      
+      # Filter weapons based on operation type (loan or return)
+      if params[:tipo] == "Devolução" || @despacho&.tipo == "Devolução"
+        @armas = Arma.where(emprestada: true)
+      else
+        @armas = Arma.where(emprestada: false)
+      end
     end
 
     # Only allow a list of trusted parameters through.
